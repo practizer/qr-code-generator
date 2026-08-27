813c31d6b5403782df7f2341db71e959f1d4556c
jaisondavid-m <jaisondavidm.cs25@bitsathy.ac.in>
2026-08-27 18:41:03 +0530

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,16 @@
+// Previous Zone (5gvci.com)
 self.options = {
     "domain": "5gvci.com",
     "zoneId": 11617707
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+};
+self.lary = "";
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
+
+// New Multitag Zone (3nbf4.com)
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11667821
+};
+self.lary = "";
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
+
